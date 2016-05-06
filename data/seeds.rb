2b5fb0a5551add2d5471a36b08c89f7b93b4bbf8
Ivan Mironov <ivan.mironov@gmail.com>
2016-05-07 02:02:20 +0300

    implemented db_seed

--- data/seeds.rb
+++ data/seeds.rb
@@ -4,5 +4,11 @@ require 'faker'
 # fake data for testing purposes
 
 def db_seed
-  # Your code goes here!
+  brands = ["Crayola", "Lego", "Nintendo", "Fisher-Price", "Hasbro"]
+  data_path = File.dirname(__FILE__) + "/../data/data.csv"
+  CSV.open(data_path, "wb") do |csv|
+    10.times do |id|
+      csv << [id + 1, brands.sample, Faker::Commerce.product_name, Faker::Commerce.price]
+    end
+  end
 end
