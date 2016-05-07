d9ebbcaf81e42d62ef28ec81f7bc1023eaf9d00d
Ivan Mironov <ivan.mironov@gmail.com>
2016-05-07 11:47:59 +0300

    ipassing test_create_method_adds_to_database

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,5 +3,18 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  # Your code goes here!
+
+  def self.create(opts = {})
+    product = Product.new(opts)
+    CSV.open(data_path, "ab") do |csv|
+      csv << [product.id, product.brand, product.name, product.price]
+    end
+    product
+  end
+
+  private
+
+  def self.data_path
+    File.dirname(__FILE__) + "/../data/data.csv"
+  end
 end
