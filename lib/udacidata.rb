dd41eac1c00c553769a76e8890f77f77d6849413
Ivan Mironov <ivan.mironov@gmail.com>
2016-05-07 12:02:32 +0300

    implemented all method

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -12,6 +12,12 @@ class Udacidata
     product
   end
 
+  def self.all
+    CSV.open(data_path, "r").read.map do |row|
+      Product.new(id: row[0], brand: row[1], name: row[2], price: row[3])
+    end
+  end
+
   private
 
   def self.data_path
