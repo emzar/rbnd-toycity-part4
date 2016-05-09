6f2934dc8a520ebf1527e9ed42421ef69f03b917
Ivan Mironov <ivan.mironov@gmail.com>
2016-05-09 15:01:18 +0300

    passing first method tests

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -4,7 +4,9 @@ require 'csv'
 
 class Udacidata
 
+  PRODUCT_CSV_HEADERS = %w[id brand product price]
   PRODUCT_KEYS = %i[id brand name price]
+  PRODUCT_MAPPINGS = PRODUCT_CSV_HEADERS.zip(PRODUCT_KEYS).to_h
 
   def self.create(opts = {})
     product = Product.new(opts)
@@ -15,12 +17,22 @@ class Udacidata
   end
 
   def self.all
-    CSV.open(data_path, 'r').read.drop(1).map { |row| Product.new(PRODUCT_KEYS.zip(row).to_h) }
+    CSV.open(data_path, 'r').read.drop(1).map { |row| Product.new(product_options(row)) }
+  end
+
+  def self.first
+    row = CSV.open(data_path, 'r', headers:true).shift
+    opts = row.to_hash.map { |k, v| [PRODUCT_MAPPINGS[k], v] }.to_h
+    Product.new(opts)
   end
 
   private
 
   def self.data_path
     File.dirname(__FILE__) + "/../data/data.csv"
+  end
+
+  def self.product_options(row)
+    PRODUCT_KEYS.zip(row).to_h
   end
 end
