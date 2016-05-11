13d38b55403238eefbc0e77d470cdf788efb0147
Ivan Mironov <ivan.mironov@gmail.com>
2016-05-12 00:30:36 +0300

    implement average_price

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,3 +1,6 @@
 module Analyzable
-  # Your code goes here!
+  def average_price(products)
+    price_sum = products.reduce(0) { |sum, product| sum + product.price }
+    (price_sum / products.length).round(2)
+  end
 end
