91d2a3057bab01f06858492e8379db80013643ae
Ivan Mironov <ivan.mironov@gmail.com>
2016-05-12 00:43:42 +0300

    passing test_print_report_returns_report_string

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -3,4 +3,8 @@ module Analyzable
     price_sum = products.reduce(0) { |sum, product| sum + product.price }
     (price_sum / products.length).round(2)
   end
+
+  def print_report(products)
+    "Average Price: $#{average_price(products)}"
+  end
 end
