65a3a1e07447cadc92c34ea999066ae801a663ed
Ivan Mironov <ivan.mironov@gmail.com>
2016-05-17 01:22:27 +0300

    Use Commerce.company_name for brands

--- data/seeds.rb
+++ data/seeds.rb
@@ -4,11 +4,10 @@ require 'faker'
 # fake data for testing purposes
 
 def db_seed
-  brands = ["Crayola", "Lego", "Nintendo", "Fisher-Price", "Hasbro"]
   data_path = File.dirname(__FILE__) + "/../data/data.csv"
   CSV.open(data_path, "ab") do |csv|
     10.times do |id|
-      csv << [id + 1, brands.sample, Faker::Commerce.product_name, Faker::Commerce.price]
+      csv << [id + 1, Commerce.company_name, Faker::Commerce.product_name, Faker::Commerce.price]
     end
   end
 end
