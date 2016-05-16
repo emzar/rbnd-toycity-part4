3c5cf667d23a7cc6b1b6b4ffe24a67d52cf85604
Ivan Mironov <ivan.mironov@gmail.com>
2016-05-17 01:28:51 +0300

    Seed db by Product create

--- data/seeds.rb
+++ data/seeds.rb
@@ -4,10 +4,11 @@ require 'faker'
 # fake data for testing purposes
 
 def db_seed
-  data_path = File.dirname(__FILE__) + "/../data/data.csv"
-  CSV.open(data_path, "ab") do |csv|
-    10.times do |id|
-      csv << [id + 1, Commerce.company_name, Faker::Commerce.product_name, Faker::Commerce.price]
-    end
+  10.times do
+    Product.create(
+      brand: Faker::Company.name,
+      name:  Faker::Commerce.product_name,
+      price: Faker::Commerce.price
+    )
   end
 end
